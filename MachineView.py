eaf6036e289207ca48811608d65cc7604b2f9fa6
naveed <naveed.ajaj@gmail.com>
2017-11-04 17:19:01 +0530

    changing array position

--- MachineView.py
+++ MachineView.py
@@ -26,10 +26,9 @@ class MachineMainView:
   def __init__(self) :
        countColors=["#ffffcc","#ffdab3","#ccffcc","#ffccff","#ccffff","#ffcccc","#ccccff","#cce4ff"];
        machColors=["#ffffb3","#ffcc99","#b3ffb3","#ffb3ff","#99ffff","#ffb3b3","#b3b3ff","#b3d7ff"];
-       #self.machines={17:Machine(),27:Machine(),22:Machine(),5:Machine(),6:Machine(),13:Machine(),19:Machine(),26:Machine()}
-       self.machines={26:Machine(),19:Machine(),13:Machine(),6:Machine(),22:Machine(),27:Machine(),17:Machine()}
+       self.machines={26:Machine(),13:Machine(),6:Machine(),5:Machine(),22:Machine(),27:Machine(),17:Machine()}
        #machines = [Machine() for i in range(8)]
-       pos = [26,19,13,6,22,27,17]
+       pos = [26,13,6,5,22,27,17]
        self.frame=Frame(root)
        for i  in range(0 , 28) :
              self.frame.columnconfigure(i, weight=1)
@@ -43,7 +42,7 @@ class MachineMainView:
               if j == 4 :
                   i=0
                   rowpos=3
-              Label(self.frame,height=2, font=("Courier bold", 60), anchor=W, bg=machColors[j] ,   text="M%d:" %(j+1) ) .grid(row=rowpos,column=i*6,sticky=W+E+N+S) 
+              Label(self.frame,height=2, font=("Courier bold", 60), anchor=W, bg=machColors[j] ,   text="%d:" %(j+1) ) .grid(row=rowpos,column=i*6,sticky=W+E+N+S) 
               Label(self.frame, anchor=W, background=countColors[i],font=("Courier bold", 60) , padx = 15 , textvariable=self.machines[pos[j]].macCycleCount).grid(row=rowpos,column=(i*6+1),columnspan=5,sticky=W+E+N+S)
               self.machines[pos[j]].lblstate1= Label(self.frame, height=2,anchor=W ,bg="#ff1a1a" ,font=("Courier bold", 40),  padx = 5 ,textvariable=self.machines[pos[j]].macCycleCounter )
               self.machines[pos[j]].lblstate1.grid(row=rowpos+1,column=i*6,sticky=W+E+N+S,columnspan=6)      
